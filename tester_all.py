a0b9a00ed0bf73b0500351de7836de70d1d0c8a7
jesseherwig <j.herwig@qut.edu.au>
2021-06-13 01:43:33 +1000

    vaccine multitest with citizens and random

---
+++ tester_all.py
@@ -0,0 +1,24 @@
+from launcher import tester
+
+import config
+
+import os
+
+output_files = '50_all_random'
+
+
+config.citizen_source = 'citizens_50k_vaccinated_random.txt'
+config.links_source = 'links_50k.txt'
+runtime = 365
+
+if __name__ == '__main__':
+    if os.path.exists(output_files + 'count.txt'):
+        with open(output_files + 'count.txt', 'r') as f:
+            count = int(f.read())
+        count += 1
+    else:
+        count = 1
+    with open(output_files + 'count.txt', 'w') as f:
+        f.write(str(count))
+    output_files += str(count)
+    tester(output_files, runtime)
