3194aaf5770d7df31ae651718a3161b00255f6d3
jesseherwig <jess270898@gmail.com>
2021-06-12 21:21:25 +1000

    finsheder

--- model.py
+++ model.py
@@ -494,13 +494,3 @@ class Population:
                 + '  Full: ' + str(self.vaccine_totals['moderna_full']) \
                 + '\n-------------------------------'
         return stats
-
-
-class Node:
-
-    def __init__(self, state, parent=None, action=None, path_cost=0):
-        self.state = state
-        self.parent = parent
-        self.depth = 0
-        if parent:
-            self.depth = parent.depth + 1
